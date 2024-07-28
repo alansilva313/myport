5569c9d1a17dbc352a8d0b5ffdbfec794c7794f7
alansilva313 <allansillva313@gmail.com>
2024-07-28 13:43:43 -0300

    name

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -20,8 +20,8 @@ function Aside(){
         <div className="flex flex-col pt-20 pb-16 items-center w-full dark:bg-black bg-slate-800 text-white pl-10 pr-10 lg:pl-36 lg:pr-36 lg:justify-between lg:text-start justify-center text-center">
           
           <div className="flex flex-col lg:text-start gap-4 r text-center">
-           <h3 className="text-4xl leading-relaxed">Olá, sou
-             <animated.div style={spring}><button className="bg-blue-600">DEV FULLSTACK</button></animated.div> Alan Silva</h3>
+           <h3 className="text-4xl leading-tight"><strong className='text-[15px] text-start'>Olá, sou</strong>
+             <animated.div style={spring}>Alan Silva <button className="bg-blue-600 pl-4 pr-4 text-[22px] rounded">DEV FULLSTACK</button></animated.div></h3>
              <animated.div style={spring2} className="mt-4">
              <p className='leading-relaxed '>Mais de 3 anos de experiência em desenvolvimento web, contruindo aplicações de forma rapida e bem estruturada!</p>
              </animated.div>

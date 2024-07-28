c8fb67bed3792583e0736408b2276e8a86d6d533
alansilva313 <allansillva313@gmail.com>
2024-07-28 14:06:39 -0300

    tr

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,6 +1,6 @@
 import { animated, useScroll } from "@react-spring/web"
 import Uptopo from "../uptopo";
-
+import projects from '../../services/api-projetos'
 
 
 function Projects()
@@ -8,42 +8,7 @@ function Projects()
       
     const { scrollXProgress }: any = useScroll({});
 
-    const projects = [
-        {
-            id: 1,
-            title: "Primeiro projeto",
-            description: "Projeto em andamento",
-            link: "#"
-        },
-        {
-            id: 2,
-            title: "Segundo projeto",
-            description: "Este é o meu segundo projeto!",
-            link: "#"
-            
-        },
-
-        {
-            id: 3,
-            title: "Terceiro projeto",
-            description: "Este é o meu Terceiro projeto!",
-            link: "#"
-        },
-        {
-            id: 4,
-            title: "Quarto projeto",
-            description: "Este é o meu Quarto projeto!",
-            link: "#"
-        },
-
-        {
-            id: 5,
-            title: "Quinto projeto",
-            description: "Este é o meu Quinto projeto!",
-            link: "#"
-        }
-    ]
- 
+  
 
     return (
         <>
---
+++ src/services/api-projetos.ts
@@ -0,0 +1,37 @@
+const projects = [
+    {
+        id: 1,
+        title: "Primeiro projeto",
+        description: "Projeto em andamento",
+        link: "#"
+    },
+    {
+        id: 2,
+        title: "Segundo projeto",
+        description: "Este é o meu segundo projeto!",
+        link: "#"
+        
+    },
+
+    {
+        id: 3,
+        title: "Terceiro projeto",
+        description: "Este é o meu Terceiro projeto!",
+        link: "#"
+    },
+    {
+        id: 4,
+        title: "Quarto projeto",
+        description: "Este é o meu Quarto projeto!",
+        link: "#"
+    },
+
+    {
+        id: 5,
+        title: "Quinto projeto",
+        description: "Este é o meu Quinto projeto!",
+        link: "#"
+    }
+]
+
+export default projects
